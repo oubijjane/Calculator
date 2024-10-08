181333b63a4e46dfd6654e7ab3f625819407508c
oubijjane <79411385+oubijjane@users.noreply.github.com>
2024-10-08 13:03:36 +0100

    add the ui for the calculator
    add the ui and link the number to the logic

--- script.js
+++ script.js
@@ -1,3 +1,23 @@
+const display = document.querySelector(".display");
+const div = document.querySelector(".numbres");
+let h = "yth"
+div.addEventListener("click", (e) => {
+    console.log(e.target.textContent.includes("."));
+    switch(e.target.className) {
+        case "number":
+            if(!display.textContent.includes(".") && e.target.textContent == ".") {
+                display.textContent += e.target.textContent;
+            } else if ( e.target.textContent !== ".") {
+            display.textContent += e.target.textContent;
+        }
+        break;
+
+        case "ac":
+            display.textContent = "";
+        break;
+    }
+})
+
 let firstNumber = 5;
 let secondNumber = 6;
 let result = 0;
